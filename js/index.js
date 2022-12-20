63740668deade639217980d44d0693c33f539334
John Doe <hwang203.1608@gmail.com>
2022-12-20 13:04:43 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/*
+   Pure css Santa Caul by
+      Hwang
+      
+   Share this pen if you like it :)
+   
+   and this is my instagram account if you want to follow me:
+   
+      @nthwang1608
+*/
